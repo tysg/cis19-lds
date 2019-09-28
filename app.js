d4da692ba0a114757658536be78a8401f9105976
joloong <joelloong@gmail.com>
2019-09-28 11:48:39 +0800

    Init Lottery

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var weddingRouter = require("./routes/wedding");
 var chessgameRouter = require("./routes/chessgame.js");
+var lotteryRouter = require("./routes/lottery");
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use("/users", usersRouter);
 // codeit suisse endpoints here
 app.use("/wedding-nightmare", weddingRouter);
 app.use("/chessgame", chessgameRouter);
+app.use("/lottery", lotteryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/wedding.js
+++ routes/wedding.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-	res.send("Wedding Nightmare");
-	console.log(req.body);
+router.post("/", function(req, res, next) {
+	var input = req.body;
+
+	for (var i = 0; i < input.length; i++) {
+		var task = input[i];
+		const testCase = task["test_case"];
+		const numOfGuests = task["guests"];
+		const numOfTables = task["tables"];
+		var friends = task["friends"];
+		var enemies = task["enemies"];
+		var families = task["families"];
+	}
 });
 
 module.exports = router;
