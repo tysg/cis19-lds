8800d89d8e83328a2e7f927bfc3ceff99b788b75
Tianyi <42670338+tysng@users.noreply.github.com>
2019-09-28 14:31:15 +0800

    Depman:Used kahn algo

--- routes/depMan.js
+++ routes/depMan.js
@@ -6,12 +6,20 @@ router.post("/", function(req, res, next) {
   const result = solution(req.body["modules"], req.body["dependencyPairs"]);
   res.send(result);
 });
-
 var graph = [];
 var items = {};
 var visited = [];
 var p = [];
 var result = [];
+var indeg = [];
+
+const sample = {
+  modules: ["m1", "m2"],
+  dependencyPairs: [
+    { dependee: "m2", dependentOn: "m1" },
+    { dependee: "m1", dependentOn: "m2" }
+  ]
+};
 
 function solution(modules, pairs) {
   if (modules.length === 0) {
@@ -23,15 +31,21 @@ function solution(modules, pairs) {
     items[modules[i]] = i;
   }
 
-  graph = new Array(modules.length);
+  indeg = new Array(modules.length);
+  indeg.fill(0);
+
   visited = new Array(modules.length);
+  visited.fill(false);
+
   p = new Array(modules.length);
+  p.fill(-1);
 
   graph = new Array(modules.length);
   for (let i = 0; i < modules.length; i++) {
     graph[i] = new Array();
   }
 
+  // generating graph
   for (let i = 0; i < pairs.length; i++) {
     const on = pairs[i].dependentOn;
     if (graph[items[on]] === undefined) {
@@ -40,16 +54,36 @@ function solution(modules, pairs) {
     graph[items[on]].push(items[pairs[i].dependee]);
   }
 
-  visited.fill(false);
-  p.fill(-1);
-
-  for (let i = 0; i < modules.length; i++) {
-    if (!visited[i]) {
-      dfs(i);
+  // popluating indeg arr
+  for (let i = 0; i < graph.length; i++) {
+    for (let j = 0; j < graph[i].length; j++) {
+      indeg[graph[i][j]]++;
     }
   }
 
-  return result.map(i => modules[i]).reverse();
+  // populating the queue
+  let q = [];
+  for (let i = 0; i < indeg.length; i++) {
+    if (indeg[i] === 0) {
+      q.push(i);
+    }
+  }
+
+  while (q.length !== 0) {
+    const element = q.shift();
+    result.push(element);
+    let arr = graph[element];
+    for (let i = 0; i < arr.length; i++) {
+      if (indeg[i] > 0) {
+        indeg[i]--;
+      } else {
+        p[i] = element;
+        q.push(i);
+      }
+    }
+  }
+
+  return result.length === 0 ? [] : result.map(i => modules[i]);
 }
 
 function dfs(ind) {
@@ -63,5 +97,7 @@ function dfs(ind) {
 
   result.push(ind);
 }
+
+// console.log(solution(sample.modules, sample.dependencyPairs));
 
 module.exports = router;
