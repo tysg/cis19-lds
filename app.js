054022378a69d7683104b0ce9c5790a06150b9d4
dingyuchen <dingyuchen@gmail.com>
2019-09-28 14:36:09 +0800

    add fix for chess and boiler for typing-contest

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var chessgameRouter = require("./routes/chessgame.js");
 var lotteryRouter = require("./routes/lottery");
 var sentimentRouter = require("./routes/sentiment");
 var depManRouter = require("./routes/depMan");
+var typingRouter = require("./routes/typing");
 
 var app = express();
 
@@ -19,8 +20,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -33,6 +34,7 @@ app.use("/chessgame", chessgameRouter);
 app.use("/lottery", lotteryRouter);
 app.use("/sentiment-analysis", sentimentRouter);
 app.use("/generateSequence", depManRouter);
+app.use("/typing-contest", typingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
