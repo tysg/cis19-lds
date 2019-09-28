3372b02490f4df585d8199229ac8584dcc1fc1c0
joloong <joelloong@gmail.com>
2019-09-28 10:47:08 +0800

    Log

--- routes/wedding.js
+++ routes/wedding.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/", function(req, res, next) {
+router.get("/", function(req, res, next) {
 	console.log(req.body);
 });
 
