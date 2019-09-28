30d07e49e6903d882b478b76e158f1a593383e4e
Tianyi <42670338+tysng@users.noreply.github.com>
2019-09-28 23:14:29 +0800

    Tokenize paragraph

--- routes/sentiment/main.py
+++ routes/sentiment/main.py
@@ -1,13 +1,26 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
 import json
 
 
 analyzer = SentimentIntensityAnalyzer()
+nltk.download("punkt")
 
 
-def analyse(sentences):
-    result = list(map(lambda x: analyzer.polarity_scores(x), sentences))
-    return list(map(lambda x: "positive" if x['compound'] > 0 else "negative", result))
+def analyse(paragraphs):
+    result = list(map(lambda x: analyse_paragraph(x), paragraphs))
+    return list(map(lambda x: "positive" if x > 0 else "negative", result))
+
+
+def analyse_paragraph(paragraph):
+    sentence_list = nltk.tokenize.sent_tokenize(paragraph)
+    paragraphSentiments = 0.0
+    for sentence in sentence_list:
+        vs = analyzer.polarity_scores(sentence)
+        # print("{:-<69} {}".format(sentence, str(vs["compound"])))
+        paragraphSentiments += vs["compound"]
+
+    return round(paragraphSentiments / len(sentence_list), 4)
 
 
 if __name__ == "__main__":
