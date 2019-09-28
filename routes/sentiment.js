1f7c0efa59e43937caa54a808f2e4a1f78c0f369
dingyuchen <dingyuchen@gmail.com>
2019-09-28 22:43:05 +0800

    change vocab to senticon

--- routes/sentiment.js
+++ routes/sentiment.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var Analyzer = require("natural").SentimentAnalyzer;
 var stemmer = require("natural").PorterStemmer;
 // var tokenizer = require("natural").WordTokenizer;
-var analyzer = new Analyzer("English", stemmer, "afinn");
+var analyzer = new Analyzer("English", stemmer, "senticon");
 
 var natural = require("natural");
 var tokenizer = new natural.WordTokenizer();
